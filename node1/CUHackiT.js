9f4eb57758ced3263f21fac7ff382beb595373b8
himanshudhawale <himanshudhawale2@icloud.com>
2020-01-26 03:37:21 -0500

    added node js

---
+++ node1/CUHackiT.js
@@ -0,0 +1,321 @@
+var log4js = require('log4js');
+var logger = log4js.getLogger('SampleWebApp');
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var util = require('util');
+var app = express();
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var bearerToken = require('express-bearer-token');
+var cors = require('cors');
+var hfc = require('fabric-client');
+var helper = require('./app/helper.js');
+var channels = require('./app/create-channel.js');
+var join = require('./app/join-channel.js');
+var install = require('./app/install-chaincode.js');
+var instantiate = require('./app/instantiate-chaincode.js');
+var invoke = require('./app/invoke-transaction.js');
+var query = require('./app/query.js');
+
+app.options('*', cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+function sleep(seconds){
+    var waitUntil = new Date().getTime() + seconds*1000;
+    while(new Date().getTime() < waitUntil) true;
+}
+
+// set secret variable
+app.set('secret', 'thisismysecret');
+// app.use(expressJWT({
+// 	secret: 'thisismysecret'
+// }).unless({
+// 	path: ['/users']
+// }));
+// app.use(bearerToken());
+
+app.use(function(req, res, next) {
+	if (req.originalUrl.indexOf('/users') >= 0) {
+		return next();
+	}
+	var token = req.token;
+	jwt.verify(token, app.get('secret'), function(err, decoded) {
+		if (err) {
+			res.send({
+				success: false,
+				message: 'Failed to authenticate token. Make sure to include the ' +
+					'token returned from /users call in the authorization header ' +
+					' as a Bearer token'
+			});
+			return;
+		} else {
+			// add the decoded user name and org name to the request object
+			// for the downstream code to use
+			req.username = decoded.username;
+			req.orgname = decoded.orgName;
+			logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
+			return next();
+		}
+	});
+});
+
+var server = http.createServer(app).listen(8080, function() {});
+logger.info('****************** SERVER STARTED ************************');
+server.timeout = 240000;
+
+function getErrorMessage(field) {
+	var response = {
+		success: false,
+		message: field + ' field is missing or Invalid in the request'
+	};
+	return response;
+}
+
+///////////////////////// REST ENDPOINTS START HERE ///////////////////////////
+// Register and enroll user
+app.post('/users', function(req, res) {
+	var username = req.body.username;
+	var orgName = req.body.orgName;
+	logger.debug('End point : /users');
+	logger.debug('User name : ' + username);
+	logger.debug('Org name  : ' + orgName);
+	if (!username) {
+		res.json(getErrorMessage('\'username\''));
+		return;
+	}
+	if (!orgName) {
+		res.json(getErrorMessage('\'orgName\''));
+		return;
+	}
+	var token = jwt.sign({
+		exp: Math.floor(Date.now() / 1000) + parseInt(hfc.getConfigSetting('jwt_expiretime')),
+		username: username,
+		orgName: orgName
+	}, app.get('secret'));
+	helper.getRegisteredUsers(username, orgName, true).then(function(response) {
+		if (response && typeof response !== 'string') {
+			response.token = token;
+			res.json(response);
+		} else {
+			res.json({
+				success: false,
+				message: response
+			});
+		}
+	});
+});
+
+// Create Channel
+app.post('/channels', function(req, res) {
+	logger.info('<<<<<<<<<<<<<<<<< C R E A T E  C H A N N E L >>>>>>>>>>>>>>>>>');
+	logger.debug('End point : /channels');
+	var channelName = req.body.channelName;
+	var channelConfigPath = req.body.channelConfigPath;
+	logger.debug('Channel name : ' + channelName);
+	logger.debug('channelConfigPath : ' + channelConfigPath); //../channel-artifacts/channel/channel1.tx
+	if (!channelName) {
+		res.json(getErrorMessage('\'channelName\''));
+		return;
+	}
+	if (!channelConfigPath) {
+		res.json(getErrorMessage('\'channelConfigPath\''));
+		return;
+	}
+
+	channels.createChannel(channelName, channelConfigPath, req.username, req.orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+});
+// Join Channel
+app.post('/channels/:channelName/peers', function(req, res) {
+	logger.info('<<<<<<<<<<<<<<<<< J O I N  C H A N N E L >>>>>>>>>>>>>>>>>');
+	var channelName = req.params.channelName;
+	var peers = req.body.peers;
+	logger.debug('channelName : ' + channelName);
+	logger.debug('peers : ' + peers);
+	if (!channelName) {
+		res.json(getErrorMessage('\'channelName\''));
+		return;
+	}
+	if (!peers || peers.length == 0) {
+		res.json(getErrorMessage('\'peers\''));
+		return;
+	}
+
+	join.joinChannel(channelName, peers, req.username, req.orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+});
+
+
+// Install chaincode on target peers
+app.post('/chaincodes', function(req, res) {
+	logger.debug('==================== INSTALL CHAINCODE ==================');
+	var peers = req.body.peers;
+	var chaincodeName = req.body.chaincodeName;
+	var chaincodePath = req.body.chaincodePath;
+	var chaincodeVersion = req.body.chaincodeVersion;
+	logger.debug('peers : ' + peers); // target peers list
+	logger.debug('chaincodeName : ' + chaincodeName);
+	logger.debug('chaincodePath  : ' + chaincodePath);
+	logger.debug('chaincodeVersion  : ' + chaincodeVersion);
+	if (!peers || peers.length == 0) {
+		res.json(getErrorMessage('\'peers\''));
+		return;
+	}
+	if (!chaincodeName) {
+		res.json(getErrorMessage('\'chaincodeName\''));
+		return;
+	}
+	if (!chaincodePath) {
+		res.json(getErrorMessage('\'chaincodePath\''));
+		return;
+	}
+	if (!chaincodeVersion) {
+		res.json(getErrorMessage('\'chaincodeVersion\''));
+		return;
+	}
+
+	install.installChaincode(peers, chaincodeName, chaincodePath, chaincodeVersion, req.username, req.orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+});
+
+// Instantiate chaincode on target peers
+// Instantiate chaincode on target peers
+app.post('/channels/:channelName/chaincodes', async function(req, res) {
+	logger.debug('==================== INSTANTIATE CHAINCODE ==================');
+	var peers = req.body.peers;
+	var chaincodeName = req.body.chaincodeName;
+	var chaincodeVersion = req.body.chaincodeVersion;
+	var channelName = req.params.channelName;
+	// var chaincodeType = req.body.chaincodeType;
+	var fcn = req.body.fcn;
+	var args = req.body.args;
+	// var args = ["CustomerA","ShipperA","12th July 2019","500", "110000012",""];
+	// args[0]="CustomerA";
+	logger.debug('args  : ' + args);
+
+	logger.debug('peers  : ' + peers);
+	logger.debug('channelName  : ' + channelName);
+	logger.debug('chaincodeName : ' + chaincodeName);
+	logger.debug('chaincodeVersion  : ' + chaincodeVersion);
+	// logger.debug('chaincodeType  : ' + chaincodeType);
+	logger.debug('fcn  : ' + fcn);
+	if (!chaincodeName) {
+		res.json(getErrorMessage('\'chaincodeName\''));
+		return;
+	}
+	if (!chaincodeVersion) {
+		res.json(getErrorMessage('\'chaincodeVersion\''));
+		return;
+	}
+	if (!channelName) {
+		res.json(getErrorMessage('\'channelName\''));
+		return;
+	}
+	// ipfs.upload(process.cwd() + '/uchiha.jpg',function(err,data){
+	// 		if (err)
+	// 				logger.debug(err);
+	// 		else
+	// 		   args.push(data);
+	// 			 logger.debug("IPFS Hash is: " + args[5]);
+	// 			 sleep(20);
+
+	// });
+
+	if (!args) {
+		res.json(getErrorMessage('\'args\''));
+		return;
+	}
+
+	let message = await instantiate.instantiateChaincode(peers, channelName, chaincodeName, chaincodeVersion, fcn, args, req.username, req.orgname);
+	res.send(message);
+	// if (!chaincodeType) {
+	// 	res.json(getErrorMessage('\'chaincodeType\''));
+	// 	return;
+	// }
+
+});
+// Invoke transaction on chaincode on target peers
+app.post('/channels/:channelName/chaincodes/:chaincodeName', function(req, res) {
+	logger.debug('==================== INVOKE ON CHAINCODE ==================');
+	var peers = req.body.peers;
+	var chaincodeName = req.params.chaincodeName;
+	var channelName = req.params.channelName;
+	var fcn = req.body.fcn;
+	var args = req.body.args;
+	logger.debug('channelName  : ' + channelName);
+	logger.debug('chaincodeName : ' + chaincodeName);
+	logger.debug('fcn  : ' + fcn);
+	logger.debug('args  : ' + args);
+	if (!chaincodeName) {
+		res.json(getErrorMessage('\'chaincodeName\''));
+		return;
+	}
+	if (!channelName) {
+		res.json(getErrorMessage('\'channelName\''));
+		return;
+	}
+	if (!fcn) {
+		res.json(getErrorMessage('\'fcn\''));
+		return;
+	}
+	if (!args) {
+		res.json(getErrorMessage('\'args\''));
+		return;
+	}
+
+	invoke.invokeChaincode(peers, channelName, chaincodeName, fcn, args, req.username, req.orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+});
+// Query by chaincode on target peers
+app.get('/channels/:channelName/chaincodes/:chaincodeName', function(req, res) {
+	logger.debug('==================== QUERY BY CHAINCODE ==================');
+	var channelName = req.params.channelName;
+	var chaincodeName = req.params.chaincodeName;
+	let args = req.query.args;
+	let fcn = req.query.fcn;
+	let peer = req.query.peer;
+
+	logger.debug('channelName : ' + channelName);
+	logger.debug('chaincodeName : ' + chaincodeName);
+	logger.debug('fcn : ' + fcn);
+	logger.debug('args : ' + args);
+
+	if (!chaincodeName) {
+		res.json(getErrorMessage('\'chaincodeName\''));
+		return;
+	}
+	if (!channelName) {
+		res.json(getErrorMessage('\'channelName\''));
+		return;
+	}
+	if (!fcn) {
+		res.json(getErrorMessage('\'fcn\''));
+		return;
+	}
+	if (!args) {
+		res.json(getErrorMessage('\'args\''));
+		return;
+	}
+	args = args.replace(/'/g, '"');
+	args = JSON.parse(args);
+	logger.debug(args);
+
+	query.queryChaincode(peer, channelName, chaincodeName, args, fcn, req.username, req.orgname)
+	.then(function(message) {
+		res.send(message);
+	});
+});
---
+++ src/chaincode/studentuniversity.go
@@ -0,0 +1,276 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type StudentUniversityContract struct {
+}
+
+type agreement struct {
+	ContractID     string  `json:"Key"`
+	StudentName    string  `json:"StudentName"`
+	UniversityName string  `json:"UniversityName"`
+	Date           string  `json:"Date"`
+	Amount         float64 `json:"Amount"`
+	Email          string  `json:"Email"`
+}
+
+func (t *StudentUniversityContract) Init(stubInterface shim.ChaincodeStubInterface) peer.Response {
+	fmt.Println("Student University Contract Initiated")
+
+	_, args := stubInterface.GetFunctionAndParameters()
+
+	if len(args) != 5 {
+		return shim.Error("Incorrect number of arguments.")
+	}
+	t.initStudentUniversity(stubInterface, args)
+	return shim.Success(nil)
+}
+
+func (t *StudentUniversityContract) Invoke(stubInterface shim.ChaincodeStubInterface) peer.Response {
+	function, args := stubInterface.GetFunctionAndParameters()
+	fmt.Println("Invoke has started running " + function)
+
+	// Handle different functions
+	if function == "initStudentUniversity" {
+		return t.initStudentUniversity(stubInterface, args)
+	} else if function == "queryByStudentEmail" {
+		return t.queryByStudentEmail(stubInterface, args)
+	} else if function == "getHistoryForStudent" {
+		return t.getHistoryForStudent(stubInterface, args)
+	} else if function == "invokeFunctionStudentUniversity" {
+		return t.invokeFunctionStudentUniversity(stubInterface, args)
+	}
+	fmt.Println("invoke did not find func: " + function) //error
+	return shim.Error("Received unknown function invocation")
+
+}
+
+func (t *StudentUniversityContract) initStudentUniversity(stubInterface shim.ChaincodeStubInterface, args []string) peer.Response {
+	var err error
+
+	if len(args) != 5 {
+		return shim.Error("Incorrect number of arguments. Expecting 5")
+	}
+	fmt.Println("- start init Student University contract")
+
+	if len(args[0]) <= 0 {
+		return shim.Error("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return shim.Error("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return shim.Error("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return shim.Error("4th argument must be a non-empty string")
+	}
+	if len(args[4]) <= 0 {
+		return shim.Error("5th argument must be a non-empty string")
+	}
+
+	studentName := strings.ToLower(args[0])
+	studentEmail := strings.ToLower(args[1])
+	currentDate := strings.ToLower(args[2])
+	amount, err := strconv.ParseFloat(args[3], 64)
+	universityName := strings.ToLower(args[4])
+	if err != nil {
+		return shim.Error("4th argument must be a numeric string")
+	}
+
+	hash := sha256.New()
+	hash.Write([]byte(studentName + universityName))
+	contractID := hex.EncodeToString(hash.Sum(nil))
+
+	studentContract := &agreement{contractID, studentName, universityName, currentDate, amount, studentEmail}
+	studentContractJSONasBytes, err := json.Marshal(studentContract)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stubInterface.PutState(contractID, studentContractJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end init studentContract" + "Transaction ID: " + stubInterface.GetTxID())
+	return shim.Success(nil)
+}
+
+//Function
+// =========================================================================================
+func (t *StudentUniversityContract) queryByStudentEmail(stubInterface shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	studentEmail := strings.ToLower(args[0])
+
+	queryString := fmt.Sprintf("{\"selector\":{\"UniversityName\":\"Clemson\",\"StudentEmail\":\"%s\"}}", studentEmail)
+
+	queryResults, err := getQueryResultForQueryString(stubInterface, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
+
+func (t *StudentUniversityContract) getHistoryForStudent(stubInterface shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	studentName := strings.ToLower(args[0])
+	universityName := strings.ToLower(args[1])
+
+	hash := sha256.New()
+	hash.Write([]byte(studentName + universityName))
+	contractID := hex.EncodeToString(hash.Sum(nil))
+
+	fmt.Printf("- start getHistoryForStudent", contractID)
+
+	resultsIterator, err := stubInterface.GetHistoryForKey(contractID)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		fmt.Printf("- getHistoryForStudent response", response)
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// if it was a delete operation on given key, then we need to set the
+		//corresponding value null. Else, we will write the response.Value
+		//as-is (as the Value itself a JSON marble)
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+
+		buffer.WriteString(", \"Timestamp\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"IsDelete\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getHistoryForStudent returning:\n%s\n", buffer.String())
+
+	return shim.Success([]byte(buffer.String()))
+}
+
+func (t *StudentUniversityContract) invokeFunctionStudentUniversity(stubInterface shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Println("Start invokeFunctionStudentUniversity")
+	var mykey, jsonResp string
+	var err error
+	mykey = args[0]
+	//newGasAmount := args[1]
+	valAsbytes, err := stubInterface.GetState(string(mykey))
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + mykey + "\"}"
+		return shim.Error(jsonResp)
+	} else if valAsbytes == nil {
+		jsonResp = "{\"Error\":\"invokeFunctionStudentUniversity does not exist: " + mykey + "\"}"
+		return shim.Error(jsonResp)
+	}
+	fmt.Println(valAsbytes)
+	MYagreement := agreement{}
+	//umarshal the data to a new ballot struct
+	_ = json.Unmarshal(valAsbytes, &MYagreement)
+
+	fmt.Println(MYagreement)
+	return shim.Success(valAsbytes)
+
+}
+
+// =========================================================================================
+func getQueryResultForQueryString(stubInterface shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("Inside getQueryResultForQueryString")
+
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stubInterface.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
+}
+
+// ===================================================================================
+// Main
+// ===================================================================================
+func main() {
+	err := shim.Start(new(StudentUniversityContract))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
